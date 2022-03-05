f2fc516185ce7c973b59dfebf003a9e1521bba0f
vinicius <vinisurf1206@gmail.com>
2022-03-04 22:32:26 -0300

    tarefa finalizada

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Quando você se foi, chorei (chorei");
+Console.ResetColor();
+Console.WriteLine("Quanta saudade senti");
+Console.WriteLine("Agora que eu virei rei");
+Console.WriteLine("Você vem querer 'tá aqui(Você vem querer 'tá aqui");
+Console.WriteLine("Tu vai perceber que eu sou um cara difícil");
+Console.WriteLine("A marra e o estilo fazem parte do ofício");
+Console.WriteLine("Esse drink eu trouxe pra gente beber");
+Console.WriteLine("Só eu e você no amanhecer");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Vendo o Sol nascer com a vista do Rio de Janeiro");
+Console.WriteLine("Um Ret bom pra fazer a vibe");
+Console.WriteLine("Traz uma caixa de som, um flow");
+Console.ResetColor();
+Console.WriteLine("E eu ganho o seu coração, uma condição");
+Console.WriteLine("E eu deixo tu dormir lá em casa");
+Console.WriteLine("Woah, ah-yeah");
+Console.WriteLine("E eu deixo tu dormir lá em casa");
